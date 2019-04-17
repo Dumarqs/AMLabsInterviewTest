be700ea7c5dc9fcc76986b302e0f5d20e2102d87
Eduardo Marques <eduardo.marques@resourceit.com>
2019-04-16 23:38:35 -0300

    Need to solve some bugs with the pagination

--- InterviewTestPagination/app/main.js
+++ InterviewTestPagination/app/main.js
@@ -4,59 +4,72 @@
     angular
         .module("todoApp")
         .directive("todoPaginatedList", [todoPaginatedList])
-        .directive("pagination", [pagination]);
 
-    /**
-     * Directive definition function of 'todoPaginatedList'.
-     * 
-     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
-     * TODO: create appropriate functions (link? controller?) and scope bindings
-     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
-     * 
-     * @returns {} directive definition object
-     */
     function todoPaginatedList() {
         var directive = {
             restrict: "E", // example setup as an element only
             templateUrl: "app/templates/todo.list.paginated.html",
             scope: {}, // example empty isolate scope
-            controller: ["$scope", "$http", controller],
-            link: link
+            controller: ["$scope", "$http", controller]
         };
 
         function controller($scope, $http) { // example controller creating the scope bindings
             $scope.all = [];
-            // example of xhr call to the server's 'RESTful' api
-            $http.get("api/Todo/All").then(response => $scope.todos = response.data);
+
+            $scope.maxSize = 5;     // Limit number for pagination display number.  
+            $scope.totalCount = 0;  // Total number of items in all pages. initialize as a zero  
+            $scope.pageIndex = 1;   // Current page number. First page is 1.-->  
+            $scope.pageSizeSelected = 20; // Maximum number of items per page. 
+
+            $scope.DropDownList = [
+                {
+                    Value: 0,
+                    Name: 'All',
+                    Selected: false
+                }, {
+                    Value: 10,
+                    Name: '10',
+                    Selected: false
+                }, {
+                    Value: 20,
+                    Name: '20',
+                    Selected: true
+                }, {
+                    Value: 30,
+                    Name: '30',
+                    Selected: false
+                }]; // Set dropdownlist values
+
+            $scope.getAll = function() {
+                $http.get("api/Todo/All").then(response => $scope.todos = response.data);
+            }
+
+            $scope.getByPage = function() {
+                $http.get("api/Todo/ByPage?page=" + $scope.pageIndex + "&pageSize=" + $scope.pageSizeSelected).then(
+                    function(response) {
+                        $scope.todos = response.data;
+                    });
+            }
+
+            //Loading todo list
+            $scope.getAll();  
+
+            //This method is called when the page is changed
+            $scope.pageChanged = function () {
+                $scope.getByPage();
+            }; 
+
+            //This method is calling from dropDown  
+            $scope.changePageSize = function () {
+                $scope.pageIndex = 1;
+                if ($scope.pageSizeSelected === "0") {
+                    $scope.getAll();
+                } else {
+                    $scope.getByPage();
+                }
+                
+            }; 
         }
-
-        function link(scope, element, attrs) { }
-
-        return directive;
-    }
-
-    /**
-     * Directive definition function of 'pagination' directive.
-     * 
-     * TODO: make it a reusable component (i.e. usable by any list of objects not just the Models.Todo model)
-     * TODO: correctly parametrize scope (inherited? isolated? which properties?)
-     * TODO: create appropriate functions (link? controller?) and scope bindings
-     * TODO: make appropriate general directive configuration (support transclusion? replace content? EAC?)
-     * 
-     * @returns {} directive definition object
-     */
-    function pagination() {
-        var directive = {
-            restrict: "E", // example setup as an element only
-            templateUrl: "app/templates/pagination.html",
-            scope: {}, // example empty isolate scope
-            controller: ["$scope", controller],
-            link: link
-        };
-
-        function controller($scope) { }
-
-        function link(scope, element, attrs) { }
 
         return directive;
     }
